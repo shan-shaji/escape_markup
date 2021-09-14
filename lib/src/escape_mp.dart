199f3b1a250efb3309d680652f55d5691d6d37f4
shan-shaji <abrahamshanshaji@gmail.com>
2021-09-14 14:27:20 +0530

    feat:  add unescape feature

--- lib/src/escape_mp.dart
+++ lib/src/escape_mp.dart
@@ -1,19 +1,69 @@
-_htmlEscape(String string) => string.replaceAll(RegExp(r"([^&])&(|&)"), ' &amp;')
-  ..replaceAll('/"/g', '&quot;')
-  ..replaceAll("/'/g", '&#39;')
-  ..replaceAll("/</g", '&lt;')
-  ..replaceAll('/>/g', '&gt;');
+String _htmlEscape(String string) {
+  if (RegExp(r'([^&])&(|&)').hasMatch(string)) {
+    string = string.replaceAll(RegExp(r"([^&])&(|&)"), ' &amp;');
+  }
 
-_htmlUnescape(htmlString) => htmlString.replaceAll('/&gt;/g', '>')
-  ..replaceAll('/&lt;/g', '<')
-  ..replaceAll('/&#0?39;/g', '\'')
-  ..replaceAll('/&quot;/g', '"')
-  ..replaceAll('/&amp;/g',
-      '&'); // Must happen last or else it will unescape other characters in the wrong order.
+  if (RegExp(r'\"').hasMatch(string)) {
+    string = string.replaceAll(RegExp(r'\"'), '&quot;');
+  }
 
-String escapeMarkup(strings) {
+  if (RegExp(r"\'").hasMatch(string)) {
+    string = string.replaceAll(RegExp("/'"), '&#39;');
+  }
+
+  if (RegExp(r'<').hasMatch(string)) {
+    string = string.replaceAll(RegExp('<'), '&lt;');
+  }
+
+  if (RegExp(r'>').hasMatch(string)) {
+    string = string.replaceAll(RegExp('>'), '&gt;');
+  }
+
+  return string;
+}
+
+String _htmlUnescape(String htmlString) {
+  if (RegExp('&gt;').hasMatch(htmlString)) {
+    htmlString = htmlString.replaceAll(RegExp('&gt;'), '>');
+  }
+
+  if (RegExp('&lt;').hasMatch(htmlString)) {
+    htmlString = htmlString.replaceAll(RegExp('&lt;'), '<');
+  }
+
+  if (RegExp('&#0?39;').hasMatch(htmlString)) {
+    htmlString = htmlString.replaceAll(RegExp('&#0?39;'), '\'');
+  }
+
+  if (RegExp('&quot;').hasMatch(htmlString)) {
+    htmlString = htmlString.replaceAll(RegExp('&quot;'), '\"');
+  }
+
+  if (RegExp('&amp;').hasMatch(htmlString)) {
+    htmlString = htmlString.replaceAll(RegExp('&amp;'), '&');
+  }
+
+  return htmlString;
+}
+
+String escapeMp(strings) {
   if (strings.runtimeType == String) {
     return _htmlEscape(strings);
+  }
+
+  var output = strings[0];
+  if (strings.runtimeType == List) {
+    for (var index = 0; index <= (strings as List).length; index++) {
+      output = output + _htmlEscape(strings[index] + strings[index + 1]);
+    }
+  }
+
+  return output;
+}
+
+String unEscapeMp(strings) {
+  if (strings.runtimeType == String) {
+    return _htmlUnescape(strings);
   }
 
   var output = strings[0];
